1c7e4cbfdbc2fb82f2eb85139c3d6e2e28314a60
Rawat750 <132699631+Rawat750@users.noreply.github.com>
2024-10-22 19:14:09 +0530

    Update surebet-finder.js
    1xbet mostbet odds

--- src/surebet-finder.js
+++ src/surebet-finder.js
@@ -2,7 +2,7 @@ const MatchesAggregator = require('./aggregator');
 const SurebetCalculator = require('./calculator');
 
 class SurebetFinder {
-  constructor(apiKey, config) {
+  constructor(apiKey, config) {26dbb54b274bd89241654cc050fd2de8
     this.config = config;
     this.aggregator = new MatchesAggregator(apiKey, config);
     this.calculator = new SurebetCalculator(null, config);
